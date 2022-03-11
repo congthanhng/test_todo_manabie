ecd3f39acc83cafd0296a7f8bc6e26de2b26080a
ticy <thanhcong.uit@gmail.com>
2022-03-11 17:19:16 +0700

    test

--- test/task_bloc_test.dart
+++ test/task_bloc_test.dart
@@ -18,126 +18,129 @@ void main() {
     _taskBloc = TaskBloc(taskProvider: _taskProvider);
   });
 
-  blocTest(
-    'emits [TaskLoadedState] when [TaskDataLoadEvent] is called',
-    build: () {
-      mockito
-          .when(_taskProvider.getTasks())
-          .thenAnswer((_) => Future.value(mockRecords));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskDataLoadEvent()),
-    expect: () => [
-      isA<TaskLoadedState>().having((state) => state.data.allTask.isNotEmpty,
-          'all task not empty: ', true),
-    ],
-  );
+  group('Task Bloc Error case',(){
+    blocTest(
+      'emits [TaskFailureState] when TaskNewEvent is called',
+      build: () {
+        mockito
+            .when(_taskProvider.insert(mockTask))
+            .thenAnswer((_) => Future.error(mockTask));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskNewEvent(mockTask)),
+      expect: () => [
+        isA<TaskFailureState>(),
+      ],
+    );
 
-  blocTest(
-    'emits [TaskAddNewSuccessState] when add new is success'
-    'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
-    build: () {
-      mockito
-          .when(_taskProvider.insert(mockTask))
-          .thenAnswer((_) => Future.value(mockTask));
-      mockito
-          .when(_taskProvider.getTasks())
-          .thenAnswer((_) => Future.value([...mockRecords, mockTask]));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskNewEvent(mockTask)),
-    expect: () => [
-      isA<TaskAddNewSuccessState>(),
-      isA<TaskLoadedState>()
-          .having((state) => state.data.allTask.length, 'all task length:', 6),
-    ],
-  );
+    blocTest(
+      'emits [TaskFailureState] when TaskUpdateEvent is called',
+      build: () {
+        mockito
+            .when(_taskProvider.update(mockTask))
+            .thenAnswer((_) => Future.error(mockTask));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskUpdateEvent(mockTask)),
+      expect: () => [
+        isA<TaskFailureState>(),
+      ],
+    );
 
-  blocTest(
-    'emits [TaskDeleteSuccessState] when TaskDeleteEvent is success'
-    'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
-    build: () {
-      mockito.when(_taskProvider.delete(2)).thenAnswer((_) {
-        var idReturn = mockRecords[2].taskId;
-        mockRecords.removeAt(2);
-        return Future.value(idReturn);
-      });
-      mockito
-          .when(_taskProvider.getTasks())
-          .thenAnswer((_) => Future.value(mockRecords));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskDeleteEvent(2)),
-    expect: () => [
-      isA<TaskDeleteSuccessState>(),
-      isA<TaskLoadedState>()
-          .having((state) => state.data.allTask.length, 'all task length:', 4),
-    ],
-  );
+    blocTest(
+      'emits [TaskFailureState] when TaskDeleteEvent is called',
+      build: () {
+        mockito
+            .when(_taskProvider.delete(2))
+            .thenAnswer((_) => Future.error(mockTask));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskDeleteEvent(2)),
+      expect: () => [
+        isA<TaskFailureState>(),
+      ],
+    );
+  });
 
-  blocTest(
-    'emits [TaskUpdateSuccessState] when add new is success'
-        'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
-    build: () {
-      mockito
-          .when(_taskProvider.update(mockRecords.first))
-          .thenAnswer((_) => Future.value(mockRecords.first.taskId));
-      mockito
-          .when(_taskProvider.getTasks())
-          .thenAnswer((_) {
-        mockRecords.first = TaskModel(taskId: mockRecords.first.taskId, title:mockRecords.first.title, isDone: !mockRecords.first.isDone );
-            return Future.value(mockRecords);
-      } );
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskUpdateEvent(mockRecords.first)),
-    expect: () => [
-      isA<TaskUpdateSuccessState>(),
-      isA<TaskLoadedState>()
-          .having((state) => state.data.allTask.first.isDone, 'Task first is done:', true),
-    ],
-  );
+  group('Task Bloc Manual case',(){
+    blocTest(
+      'emits [TaskLoadedState] when [TaskDataLoadEvent] is called',
+      build: () {
+        mockito
+            .when(_taskProvider.getTasks())
+            .thenAnswer((_) => Future.value(mockRecords));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskDataLoadEvent()),
+      expect: () => [
+        isA<TaskLoadedState>().having((state) => state.data.allTask.isNotEmpty,
+            'all task not empty: ', true),
+      ],
+    );
 
-  blocTest(
-    'emits [TaskFailureState] when TaskNewEvent is called',
-    build: () {
-      mockito
-          .when(_taskProvider.insert(mockTask))
-          .thenAnswer((_) => Future.error(mockTask));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskNewEvent(mockTask)),
-    expect: () => [
-      isA<TaskFailureState>(),
-    ],
-  );
+    blocTest(
+      'emits [TaskAddNewSuccessState] when add new is success'
+          'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
+      build: () {
+        mockito
+            .when(_taskProvider.insert(mockTask))
+            .thenAnswer((_) => Future.value(mockTask));
+        mockito
+            .when(_taskProvider.getTasks())
+            .thenAnswer((_) => Future.value([...mockRecords, mockTask]));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskNewEvent(mockTask)),
+      expect: () => [
+        isA<TaskAddNewSuccessState>(),
+        isA<TaskLoadedState>()
+            .having((state) => state.data.allTask.length, 'all task length:', 6),
+      ],
+    );
 
-  blocTest(
-    'emits [TaskFailureState] when TaskUpdateEvent is called',
-    build: () {
-      mockito
-          .when(_taskProvider.update(mockTask))
-          .thenAnswer((_) => Future.error(mockTask));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskUpdateEvent(mockTask)),
-    expect: () => [
-      isA<TaskFailureState>(),
-    ],
-  );
+    blocTest(
+      'emits [TaskDeleteSuccessState] when TaskDeleteEvent is success'
+          'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
+      build: () {
+        mockito.when(_taskProvider.delete(2)).thenAnswer((_) {
+          var idReturn = mockRecords[2].taskId;
+          mockRecords.removeAt(2);
+          return Future.value(idReturn);
+        });
+        mockito
+            .when(_taskProvider.getTasks())
+            .thenAnswer((_) => Future.value(mockRecords));
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskDeleteEvent(2)),
+      expect: () => [
+        isA<TaskDeleteSuccessState>(),
+        isA<TaskLoadedState>()
+            .having((state) => state.data.allTask.length, 'all task length:', 4),
+      ],
+    );
 
-  blocTest(
-    'emits [TaskFailureState] when TaskDeleteEvent is called',
-    build: () {
-      mockito
-          .when(_taskProvider.delete(2))
-          .thenAnswer((_) => Future.error(mockTask));
-      return _taskBloc;
-    },
-    act: (bloc) => _taskBloc.add(TaskDeleteEvent(2)),
-    expect: () => [
-      isA<TaskFailureState>(),
-    ],
-  );
-
+    blocTest(
+      'emits [TaskUpdateSuccessState] when add new is success'
+          'and emit [TaskLoadedState] after TaskLoadedState load again data from DB',
+      build: () {
+        mockito
+            .when(_taskProvider.update(mockRecords.first))
+            .thenAnswer((_) => Future.value(mockRecords.first.taskId));
+        mockito
+            .when(_taskProvider.getTasks())
+            .thenAnswer((_) {
+          mockRecords.first = TaskModel(taskId: mockRecords.first.taskId, title:mockRecords.first.title, isDone: !mockRecords.first.isDone );
+          return Future.value(mockRecords);
+        } );
+        return _taskBloc;
+      },
+      act: (bloc) => _taskBloc.add(TaskUpdateEvent(mockRecords.first)),
+      expect: () => [
+        isA<TaskUpdateSuccessState>(),
+        isA<TaskLoadedState>()
+            .having((state) => state.data.allTask.first.isDone, 'Task first is done:', true),
+      ],
+    );
+  });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,25 +4,50 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test_todo_manabie/data/providers/database.dart';
+import 'package:test_todo_manabie/data/providers/impl/task_provider_impl.dart';
+import 'package:test_todo_manabie/data/providers/task_provider.dart';
 import 'package:test_todo_manabie/shelf/all_import.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:test_todo_manabie/views/screens/home/bloc/task_bloc.dart';
 
+import 'mock_data.dart';
+
+class MockTaskBloc extends MockBloc<TaskStateFake,TaskEventFake> implements TaskBloc{}
+
+class TaskStateFake extends Fake implements TaskState {}
+
+class TaskEventFake extends Fake implements TaskEvent {}
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(TodoApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+  late TaskProvider taskProvider;
+  setUp(() async {
+    // await TodoDB.open('todo.db');
+
+    Get.put<AppRoute>(AppRoute());
+    // Get.lazyPut<TaskProvider>(()=>TaskProviderImpl());
+  });
+  // taskProvider = Get.put<TaskProvider>(TaskProviderImpl());
+  testWidgets('current route is "all" page', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const TodoApp());
+    await tester.pump();
+    // await tester.pump();
+    // when(taskProvider.getTasks()).thenAnswer((_)=>Future.value(mockRecords));
+    //
+    // // Verify that our counter starts at 0.
+    // expect(find.text('all'.tr), findsNWidgets(2));
+    // expect(find.text('notifyErrorTask'.tr), findsOneWidget);
+    // expect(find.text('1'), findsNothing);
+    //
+    // // Tap the '+' icon and trigger a frame.
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
+    //
+    // // Verify that our counter has incremented.
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
+  });
 }
